4297f45d871c3f2a0f751ccf72bdec163a727a48
michael davis <davis_michael.89@yahoo.com>
2022-05-12 11:39:19 -0700

    Adding new exercises

---
+++ js-fundamentals/conventions.js
@@ -0,0 +1,49 @@
+//JS Conventions
+
+//Casing - camelCased
+var myLocationIsInArizona = true
+
+//Whitespace  - for simplicity of reading you can use it in this instance
+for(var i = 0;i < 10; i++){
+
+}
+
+//naming
+var firstName = "Michael"
+var lastName = "Davis"
+
+var friends = []
+var isDone = true
+var isGameOver = false
+
+function sum(num1, num2){
+
+}
+
+//braces
+var person = {
+    address: {
+
+    }
+}
+
+//tab (indenting)
+if(2 === 2){
+    console.log("They are equal")
+    if(3 === 3 {
+        console.log("These are also equal")
+    })
+}
+
+//HTML tags
+    // ensure they are lower case
+
+//comments
+/* 
+ kfjv fkjv ofveov oe vjofv jfogeghj
+*/
+
+//unix casing
+    // silly cia - no
+    // Silly Cia - no
+    // Silly_Cia - yes
