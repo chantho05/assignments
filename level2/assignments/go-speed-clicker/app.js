badb02b06b39a4e3f2b787d3931e2da1caa752e5
michael davis <davis_michael.89@yahoo.com>
2022-06-08 15:34:38 -0700

    new assignment

---
+++ level2/assignments/go-speed-clicker/app.js
@@ -0,0 +1,14 @@
+let num = 0;
+
+if (localStorage.getItem('click') !== null) {
+    num = Number(localStorage.click);
+}
+
+const pageClicked = () => {
+    num++;
+    localStorage.setItem("click", num);
+    console.log(num);
+}
+
+window.addEventListener("click", pageClicked);
+localStorage.clear(num)
