f918e74a8a9bda8e2256a862f64985fa249611d7
michael davis <davis_michael.89@yahoo.com>
2022-06-20 12:15:02 -0700

    removed comments from app

--- level2/assignments/princess-in-another-castle/app.js
+++ level2/assignments/princess-in-another-castle/app.js
@@ -1,90 +1,90 @@
-// //Player stat
-// class Player {
-//     constructor(name, totalCoins, status, hasStar) {
-//         this.name = name
-//         this.totalCoins = 0
-//         this.status = "Small"
-//         this.hasStar = hasStar
-//         this.gameON = true
+//Player stat
+class Player {
+    constructor(name, totalCoins, status, hasStar) {
+        this.name = name
+        this.totalCoins = 0
+        this.status = "Small"
+        this.hasStar = hasStar
+        this.gameON = true
 
-//     }
+    }
 
-//     //From created player below
-//     setName(namePicked) {
-//         this.name = namePicked
-//     }
+    //From created player below
+    setName(namePicked) {
+        this.name = namePicked
+    }
 
-//     gotHit() {
+    gotHit() {
 
-//         if (this.status === "Powered Up") {
-//             this.status = "Big"
-//         } else if (this.status === "Big") {
-//             this.status = "Small"
-//         } else {
-//             this.status = "Dead"
-//             this.gameON = false
-//         }
-//     }
+        if (this.status === "Powered Up") {
+            this.status = "Big"
+        } else if (this.status === "Big") {
+            this.status = "Small"
+        } else {
+            this.status = "Dead"
+            this.gameON = false
+        }
+    }
 
-//     gotPowerUp() {
-//         this.hasStar = false
-//         if (this.status === "Small") {
-//             this.status = "Big"
-//         } else if (this.status === "Big") {
-//             this.status = "Powered Up"
-//         } else if (this.status === "Powered Up") {
-//             this.hasStar = true
-//             console.log("Your star has you protected")
-//         }
-
-
-//     }
-
-//     addCoin() {
-//         this.totalCoins += 1
-//     }
-
-//     print() {
-//         console.log(`Star: ${this.hasStar}\n`)
-//         console.log(`Name: ${this.name}`)
-//         console.log(`Status: ${this.status}`)
-//         console.log(`Total Coins: ${this.totalCoins}`)
-//     }
-// }
-
-// const user = new Player("Luigi")
-
-// const savePrincess = (user) => {
-//     //math.floor will not work
-//     let random = Math.ceil(Math.random() * 3)
-//     if (user.status === "Dead") {
-//         clearInterval(start)
-//     } else if (random === 1) {
-//         user.gotHit()
-//         user.print()
-//     } else if (random === 2) {
-//         user.addCoin()
-//         user.print()
-
-//     } else if (random === 3) {
-//         user.gotPowerUp()
-//         user.print()
-
-//     }
-// }
-// const start = setInterval(() => savePrincess(user), 1100)
+    gotPowerUp() {
+        this.hasStar = false
+        if (this.status === "Small") {
+            this.status = "Big"
+        } else if (this.status === "Big") {
+            this.status = "Powered Up"
+        } else if (this.status === "Powered Up") {
+            this.hasStar = true
+            console.log("Your star has you protected")
+        }
 
 
-const people = ["John", "Adam", "Amber"]
+    }
 
-function peopleElements(arr) {
-    // < insert your code here!! >
-    //return array with h1
-    const newArr = arr.map(person => {
-        return `<h1> ${person} </h1>`
-    })
-    return newArr
+    addCoin() {
+        this.totalCoins += 1
+    }
+
+    print() {
+        console.log(`Star: ${this.hasStar}\n`)
+        console.log(`Name: ${this.name}`)
+        console.log(`Status: ${this.status}`)
+        console.log(`Total Coins: ${this.totalCoins}`)
+    }
 }
+
+const user = new Player("Luigi")
+
+const savePrincess = (user) => {
+    //math.floor will not work
+    let random = Math.ceil(Math.random() * 3)
+    if (user.status === "Dead") {
+        clearInterval(start)
+    } else if (random === 1) {
+        user.gotHit()
+        user.print()
+    } else if (random === 2) {
+        user.addCoin()
+        user.print()
+
+    } else if (random === 3) {
+        user.gotPowerUp()
+        user.print()
+
+    }
+}
+const start = setInterval(() => savePrincess(user), 1100)
+
+
+// const people = ["John", "Adam", "Amber"]
+
+// function peopleElements(arr) {
+//     // < insert your code here!! >
+//     //return array with h1
+//     const newArr = arr.map(person => {
+//         return `<h1> ${person} </h1>`
+//     })
+//     return newArr
+// }
 
 
 
