7405a236673b3e7d6a91835f3fb7e442e87beeb6
michael davis <davis_michael.89@yahoo.com>
2022-08-18 14:33:08 -0700

    updated

--- level3/assignments/meme-generator/src/components/Header.js
+++ level3/assignments/meme-generator/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function Header(){
-    return(
-        <header className="header">
-            <img className="header--image" src="https://pngset.com/images/meme-sticker-messages-sticker-6-confused-troll-face-meme-drawing-art-person-human-transparent-png-1546336.png" />
-            <h2 className="header--title">Meme Generator</h2>
-            <h3 className="header--project">React Course - Project 3</h3>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header--image"
+        src="https://pngset.com/images/meme-sticker-messages-sticker-6-confused-troll-face-meme-drawing-art-person-human-transparent-png-1546336.png"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+    </header>
+  );
 }
-
